a8196433b61441dfa2dc282058e28bbbb3e700be
Артур Мухайлов <arthurcloud@yandex.ru>
2020-03-02 23:51:54 +0500

    Добавьте файлы проекта.

---
+++ LinqXml50/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinqXml50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "input.xml";
+            string ouput = "ouput.xml";
+
+            XDocument d = XDocument.Load(input);
+
+            d.Root.AddFirst(new XElement("total-time",
+             d.Root.Elements()
+             .Select(e => (TimeSpan?)e.Attribute("time") ??
+             new TimeSpan(24, 0, 0))
+             .Aggregate(TimeSpan.Zero, (a, e) => a + e)));
+
+            d.Save(ouput);
+
+            Process.Start(input);
+            Process.Start(ouput);
+        }
+    }
+}
